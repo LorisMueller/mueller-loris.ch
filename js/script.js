1b99255b73ef8f923c09ca2a3e8ead8c9eb32336
LorisMueller <73881928+LorisMueller@users.noreply.github.com>
2022-06-28 18:17:20 +0200

    Added current website status
    The first Version of the Website is already finished!

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+
+window.addEventListener('scroll', function () {
+    console.log('test')
+    var navbar = document.getElementById("navbar");
+    console.log(navbar)
+    if (window.scrollY > 600) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
+
+
+//TypeIt Animation
+
+/* document.addEventListener('DOMContentLoaded', function() {
+    new TypeIt('#first-type', { //content
+            strings: ['Meine Mini-Projete'],
+            loop: [true],
+            loopDelay: 3500,
+            nextStringDelay: 750
+        })
+        .pause(200)
+        .move(-2)
+        .type("k")
+        .move(+2)
+        .type(': Frontend')
+        .go();
+});
+
+function setVw() {
+    let vw = document.documentElement.clientWidth / 100;
+    document.documentElement.style.setProperty('--vw', `${vw}px`);
+}
+ */
+
+
+
+
